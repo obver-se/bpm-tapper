fa304bce77d68f3b435172a0813a3832abb69c99
obver-se <o@obver.se>
2020-05-30 20:06:57 -0700

    Updates

---
+++ precache-manifest.22e93e50fef1eeaebaa32bf88a631960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b93e8432aa06f6c6c7a818c09f41d86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0220ea6aa62451f6b7dd",
+    "url": "/static/css/main.e0d2243a.chunk.css"
+  },
+  {
+    "revision": "d27105349ea2ca9fcc3b",
+    "url": "/static/js/2.64c609ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64c609ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0220ea6aa62451f6b7dd",
+    "url": "/static/js/main.44a327e2.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
